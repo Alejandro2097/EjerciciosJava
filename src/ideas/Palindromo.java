630b4d5dad23b6a98d7e9d6cb98887f0472c418b
Alejandro Huertas <alejo12381@hotmail.es>
2021-05-09 15:37:05 -0500

    First commit

---
+++ src/ideas/Palindromo.java
@@ -0,0 +1,29 @@
+package ideas;
+
+import java.util.Scanner;
+
+public class Palindromo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese la cadena a validar");
+		String pali = sc.next();
+		String normal = "";
+		String reverse = "";
+		for(int i = 0; i < pali.length(); i++) {
+			normal += pali.charAt(i);
+		}
+		System.out.println(normal);
+		System.out.println(pali.length());
+		for(int j = pali.length() -1  ; j >=0  ; j--) {
+			reverse += pali.charAt(j);
+		}
+		System.out.println(reverse);
+		
+		if( normal.equals(reverse)) {
+			System.out.println("La cadena ingresada es palindromo");
+		}else {
+			System.out.println("La cadena ingresada no es palindromo");
+		}
+	}
+	
+}
